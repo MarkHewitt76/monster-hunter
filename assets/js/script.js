898b37f3dce57227aad3cef539fdf6eff93fdbef
MarkHewitt76 <markyhewitt@gmail.com>
2022-03-24 04:32:49 +0000

    Add code to read a random monster from the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,18 +24,30 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+/**
+ * Displays ONLY the monster tables when the
+ * 'Meet the Monsters' button is clicked
+ */
 function displayAllMonsters() {
     hideMutableChildren();
     let allMonsters = document.getElementById("all-monsters");
     allMonsters.style.display = "";
 }
 
+/**
+ * Displays ONLY the list of rules when the
+ * 'Read the Rules' button is clicked
+ */
 function displayRules() {
     hideMutableChildren();
     let rules = document.getElementById("rules");
     rules.style.display = "";
 }
 
+/**
+ * Hides all elements with the class of
+ * "mutable-child"
+ */
 function hideMutableChildren() {
     let hiddenElements = document.getElementsByClassName("mutable-child");
     for (let element of hiddenElements) {
@@ -45,15 +57,27 @@ function hideMutableChildren() {
     }
 }
 
+/**
+ * 
+ */
 function startGame() {
     hideMainDivs();
+
     let mainGame = document.getElementById("game-main");
     let arena = document.getElementById("arena");
     mainGame.style.display = "";
     arena.style.display = "";
-    console.log('start game');
+
+    document.getElementById("level").textContent = "1";
+    document.getElementById("attacks").textContent = "3";
+
+    displayRandomMonster();
 }
 
+/**
+ * Hides all elements with the class of
+ * "mutable"
+ */
 function hideMainDivs() {
     let hiddenDivs = document.getElementsByClassName("mutable");
     for (let mutableDiv of hiddenDivs) {
@@ -63,16 +87,66 @@ function hideMainDivs() {
     }
 }
 
+/**
+ * Reads a monster table from the DOM based on the 
+ * current level and returns it as an array of objects
+ */
 function getMonsterArray() {
+    let table;
 
+    let level = document.getElementById("level").textContent;
+    if (level === "1") {
+        table = document.getElementById("level-one-monsters");
+    } else if (level === "2") {
+        table = document.getElementById("level-two-monsters");
+    } else if (level === "3") {
+        table = document.getElementById("level-three-monsters");
+    } else {
+        alert(`Error! Undefined level: ${level}. Please refresh the page.`);
+        throw `Error! Undefined level: ${level}. Aborting!`;
+    }
+
+    let tbody = table.getElementsByTagName('tbody')[0];
+    let rows = tbody.children;
+    
+    let monsters = [];
+ 
+    for (let row of rows) {
+        let monster = {};
+        
+        let cells = row.children;
+        monster.name = cells[0].textContent;
+        monster.image = cells[1].innerHTML;
+        let paragraphs = cells[2].children;
+        let weaknesses = [];
+        for (let p of paragraphs) {
+            let weakness = p.textContent;
+            weaknesses.push(weakness);
+        }
+        
+        monster.weakness = weaknesses;
+        monsters.push(monster);
+    }
+    
+    return monsters;
 }
 
+/**
+ * Takes the array returned by getMonsterArray()
+ * and uses Math.random to return a single,
+ * random monster object
+ */
 function getRandomMonster() {
-
+    let monsters = getMonsterArray();
+    return monsters[Math.floor(Math.random() * monsters.length)];
 }
 
+/**
+ * 
+ */
 function displayRandomMonster() {
-
+    let randomMonster = getRandomMonster();
+    console.log(randomMonster);
 }
 
 function resolveBattle(weapon) {
