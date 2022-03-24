11ed1841ef6c9ac81ce64057e248d21185e51ade
MarkHewitt76 <markyhewitt@gmail.com>
2022-03-24 00:11:59 +0000

    Set up functions structure

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+function displayAllMonsters() {
+
+}
+
+function displayRules() {
+
+}
+
+function hideDiv() {
+    
+}
+
+
+function startGame() {
+
+}
+
+function getMonsterArray() {
+
+}
+
+function getRandomMonster() {
+
+}
+
+function displayRandomMonster() {
+
+}
+
+function chooseWeapon() {
+
+}
+
+function resolveBattle() {
+
+}
+
+function decrementAttacks() {
+
+}
+
+function displayFailureMessage() {
+
+}
+
+function failureMessage1() {
+
+}
+
+function failureMessage2() {
+
+}
+
+function defeatMessage() {
+
+}
+
+function displayWinMessage() {
+
+}
+
+function winMessage1() {
+    
+}
+
+function winMessage2() {
+
+}
+
+function victoryMessage() {
+
+}
+
+function nextLevel() {
+
+}
+
+function incrementLevel() {
+
+}
+
+function quit() {
+
+}
+
+
+
