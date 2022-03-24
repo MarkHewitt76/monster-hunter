4ba8112cec4760014ffb61a98804e7e894ea6548
MarkHewitt76 <markyhewitt@gmail.com>
2022-03-24 01:14:25 +0000

    Add event listeners for documentt loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
-function displayAllMonsters() {
+// Wait for the DOM to finish loading before running
+// Get the elements with a class of "btn" & add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByClassName("btn");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if ((this.id === "start-btn") || (this.id === "restart-btn")) {
+                startGame();
+            } else if (this.id === "monsters-btn") {
+                displayAllMonsters();
+            } else if (this.id === "rules-btn") {
+                displayRules();
+            } else if (this.id === "next-level-btn") {
+                nextLevel();
+            } else if (this.id === "quit-btn") {
+                quit();
+            } else {
+                chooseWeapon();
+            }
+        })
+    }
+})
+
+function displayAllMonsters() {
+    console.log('display monsters');
 }
 
 function displayRules() {
-
+    console.log('display rules');
 }
 
 function hideDiv() {
@@ -12,7 +36,7 @@ function hideDiv() {
 
 
 function startGame() {
-
+    console.log('start game');
 }
 
 function getMonsterArray() {
@@ -28,7 +52,7 @@ function displayRandomMonster() {
 }
 
 function chooseWeapon() {
-
+    console.log('choose weapon');
 }
 
 function resolveBattle() {
@@ -72,7 +96,7 @@ function victoryMessage() {
 }
 
 function nextLevel() {
-
+    console.log('next level');
 }
 
 function incrementLevel() {
@@ -80,8 +104,5 @@ function incrementLevel() {
 }
 
 function quit() {
-
+    console.log('quit');
 }
-
-
-
